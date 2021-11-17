2b13bc27a975ac55d696fe603c9bfd5ea2c58515
sandole <sandole97@gmail.com>
2021-11-17 01:41:55 -0700

    modularized footer and header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,22 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import ProductScreen from "./screens/ProductScreen"
-import HomeScreen from "./screens/Homescreen"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <BrowserRouter>
       <div className="grid-container">
-        <header className="row">
-          <div>
-            <a className="brand" href="/">
-              amazona
-            </a>
-          </div>
-          <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
-          </div>
-        </header>
+        <Header/>
         <main>
-          <Route exact path="/product/:id" component={ProductScreen}></Route>
-          <Route exact path="/" component={HomeScreen}></Route>
+          <Routes>
+            <Route path="/product/:id" component={ProductScreen}></Route>
+            <Route path="/" component={HomeScreen}></Route>
+          </Routes>
         </main>
-        <footer className="row center">All rights reserved @ John Seong</footer>
+        <Footer/>
       </div>
     </BrowserRouter>
   );
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            amazona
+          </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+    </div>
+  );
+}
