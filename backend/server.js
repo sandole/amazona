9761579b2e134db8d9a3362c21e2d3d3d331245b
sandole <sandole97@gmail.com>
2021-11-26 17:11:45 -0700

    add mongo and related modules

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
 app.get('/api/products/:id', (req, res) => {
     const product = data.products.find((x) => x._id === req.params.id);
-    
     if (product) {
       res.send(product);
     } else {
@@ -13,12 +20,14 @@ app.get('/api/products/:id', (req, res) => {
     }
   });
 
+
 app.get('/api/products', (req,res) => {
     res.send(data.products);
 });
 
-app.get('/',(req,res) => {
-    res.send('Server is ready');
+app.use('/api/users', userRouter);
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
