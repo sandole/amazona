c90b66067df0f01a667d043167741a8f94e7c6c3
sandole <sandole97@gmail.com>
2021-11-27 15:02:36 -0700

    data fetched from mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,26 +2,15 @@ import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
 
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona');
 
-app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find((x) => x._id === req.params.id);
-    if (product) {
-      res.send(product);
-    } else {
-      res.status(404).send({ message: 'Product Not Found' });
-    }
-  });
-
-
-app.get('/api/products', (req,res) => {
-    res.send(data.products);
-});
-
 app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
