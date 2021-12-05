d532470bd17a0499651a09f65610c9f62be01abb
sandole <sandole97@gmail.com>
2021-12-04 17:03:01 -0700

    no change

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import productRouter from './routers/productRouter.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
