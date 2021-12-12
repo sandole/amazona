475ac042d1cd8cef043ac8e459dbcd1e0daf43a7
John Seong <sandole97@gmail.com>
2021-12-11 22:21:51 -0700

    implement place order API

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
+import orderRouter from './routers/orderRouter.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona');
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
