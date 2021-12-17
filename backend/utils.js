7633617ab1644bcd42306c1243fe78bf4d8432a7
John Seong <sandole97@gmail.com>
2021-12-17 13:51:45 -0700

    bugfix

--- backend/utils.js
+++ backend/utils.js
@@ -18,10 +18,10 @@ export const generateToken = (user) => {
 export const isAuth = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (authorization) {
-    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+    const token = authorization.slice(7, authorization.length);
     jwt.verify(
       token,
-      process.env.JWT_SECRET || 'somethingsecret',
+      process.env.JWT_SECRET || 'secret',
       (err, decode) => {
         if (err) {
           res.status(401).send({ message: 'Invalid Token' });
