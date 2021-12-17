4e97ab827e2b75bf7458e6a30a8ae6c7be8ba666
John Seong <sandole97@gmail.com>
2021-12-17 14:44:35 -0700

    implement place order

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,7 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
+import { orderCreateReducer } from './reducers/orderReducers';
 import {
   productDetailsReducer,
   productListReducer,
@@ -9,6 +10,7 @@ import {
   userRegisterReducer,
   userSigninReducer,
 } from './reducers/userReducers';
+
 
 const initialState = {
   userSignin: {
@@ -33,6 +35,7 @@ const reducer = combineReducers({
   cart: cartReducer,
   userSignin: userSigninReducer,
   userRegister: userRegisterReducer,
+  orderCreate: orderCreateReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
