e5bd685962072e71143e270e59f94c4eefca6ba9
John Seong <sandole97@gmail.com>
2021-12-17 22:43:48 -0700

    implement paypal button

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,9 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona');
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -8,7 +8,7 @@ import {
     ORDER_DETAILS_SUCCESS,
 } from '../constants/orderConstants';
 
-export const orderCreateReducer = (state = {}, action) => {
+export const orderDetailsReducer = (state = { loading: true }, action) => {
     switch (action.type) {
         case ORDER_CREATE_REQUEST:
             return { loading: true };
